687b5ceae7e38d5bac3962cb81fdb6b9c8f91513
Emerson Vieira <emevieira@gmail.com>
2021-03-08 19:30:00 -0400

    Atualizado o hover dos Cards de softKills

--- src/pages/Components/Cards/skillCard.js
+++ src/pages/Components/Cards/skillCard.js
@@ -3,45 +3,61 @@ const SkillCard = () => {
   return (
     <>
       <div className="skillsCard">
-        <img src="/assets/logo-html.png" alt="" />
-        <b>HTML</b>
-        <p>Tempo de Experiencia</p>
+        <div className="cardCentral">
+          <img src="/assets/logo-html.png" alt="" />
+          <b>HTML</b>
+          <p>Tempo de Experiencia</p>
+        </div>
       </div>
       <div className="skillsCard">
-        <img src="/assets/logo-css.png" alt="" />
-        <b>CSS</b>
-        <p>Tempo de Experiencia</p>
+        <div className="cardCentral">
+          <img src="/assets/logo-css.png" alt="" />
+          <b>CSS</b>
+          <p>Tempo de Experiencia</p>
+        </div>
       </div>
       <div className="skillsCard">
-        <img src="/assets/javascript.png" alt="" />
-        <b>JavaScript</b>
-        <p>Tempo de Experiencia</p>
+        <div className="cardCentral">
+          <img src="/assets/javascript.png" alt="" />
+          <b>JavaScript</b>
+          <p>Tempo de Experiencia</p>
+        </div>
       </div>
       <div className="skillsCard">
-        <img src="/assets/React.png" alt="" />
-        <b>ReactJS</b>
-        <p>Tempo de Experiencia</p>
+        <div className="cardCentral">
+          <img src="/assets/React.png" alt="" />
+          <b>ReactJS</b>
+          <p>Tempo de Experiencia</p>
+        </div>
       </div>
 
       <div className="skillsCard">
-        <img src="/assets/flutter.svg" alt="" />
-        <b>Flutter</b>
-        <p>Tempo de Experiencia</p>
+        <div className="cardCentral">
+          <img src="/assets/flutter.svg" alt="" />
+          <b>Flutter</b>
+          <p>Tempo de Experiencia</p>
+        </div>
       </div>
       <div className="skillsCard">
-        <img src="/assets/logo-csharp.svg" alt="" />
-        <b>C#</b>
-        <p>Tempo de Experiencia</p>
+        <div className="cardCentral">
+          <img src="/assets/logo-csharp.svg" alt="" />
+          <b>C#</b>
+          <p>Tempo de Experiencia</p>
+        </div>
       </div>
       <div className="skillsCard">
-        <img src="/assets/MySQL.png" alt="" />
-        <b>MySQL</b>
-        <p>Tempo de Experiencia</p>
+        <div className="cardCentral">
+          <img src="/assets/MySQL.png" alt="" />
+          <b>MySQL</b>
+          <p>Tempo de Experiencia</p>
+        </div>
       </div>
       <div className="skillsCard">
-        <img src="/assets/sql-server.png" alt="" />
-        <b>SQL Server</b>
-        <p>Tempo de Experiencia</p>
+        <div className="cardCentral">
+          <img src="/assets/sql-server.png" alt="" />
+          <b>SQL Server</b>
+          <p>Tempo de Experiencia</p>
+        </div>
       </div>
     </>
   );
